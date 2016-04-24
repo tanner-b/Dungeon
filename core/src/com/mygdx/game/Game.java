569b3f264b2e2c48ebe27dc74af59f771f47810d
Tanner Bergeron <tannerjbergeron@powergate.ca>
2016-04-24 14:13:20 -0400

    Added goblin and initial project setup

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -4,24 +4,33 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Sprite player;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		img = new Texture("goblin2.png");
+		player = new Sprite(img);
+		player.scale(5);
+		player.setOriginCenter();
+		player.setPosition(Gdx.graphics.getWidth() / 2 - player.getWidth() / 2,
+				  		   Gdx.graphics.getHeight() / 2 - player.getHeight() / 2);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		player.draw(batch);
 		batch.end();
 	}
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Dungeon -Alpha 0.01";
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new Game(), config);
 	}
 }
